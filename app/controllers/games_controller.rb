1f656aa54f767dcd92d480808df9837a3f116661
Piotrek Karczewski <p.karczewsk@gmail.com>
2019-07-10 13:19:36 +0200

    basic functionality for adding games

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,29 @@
 class GamesController < ApplicationController
+  def new
+    @game = Game.new
+  end
+
   def index
+  end
+
+  def show
+    @game = Game.find(params[:id])
+    render html: "Title: #{@game.title} Description: #{@game.description}"
+  end
+
+  def create
+    @game = Game.new(game_params)
+    if @game.save
+      redirect_to @game
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:title, :description, :rating)
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :games
-  get 'games/index'
   root "games#index"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
