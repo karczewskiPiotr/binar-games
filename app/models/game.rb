c6fe0eaeb5930d39994b9bd430437244fb9dbfb1
Piotrek Karczewski <p.karczewsk@gmail.com>
2019-07-11 11:39:26 +0200

    file validations

--- app/models/game.rb
+++ app/models/game.rb
@@ -4,27 +4,32 @@ class Game < ApplicationRecord
 
     validates :title, presence: true, length: { maximum: 35 }
     validates :description, presence: true, length: {maximum: 255}
-    validates_inclusion_of :rating, in: 1..10
-    validate :correct_picture_mime_type
-    validate :correct_game_guide_mime_type
+    validate :pictures_amount_ng_than_five
+    validate :pictures_extensions
+    validate :guide_extension
 
     private
 
-    def correct_picture_mime_type
-        return unless pictures.attached?
-
+    def pictures_extensions
         pictures.each do |picture|
             unless picture.content_type.in?(%w(image/jpeg image/png))
                 picture.purge
-                errors.add(:picture, 'Must be a JPG or a PNG file')
+                errors.add(:pictures, 'must be a JPG or a PNG file.')
             end
         end
     end
 
-    def correct_game_guide_mime_type
+    def guide_extension
         if game_guide.attached? && !game_guide.content_type.in?(%w(application/pdf application/msdoc))
             game_guide.purge
-            errors.add(:game_guide, 'Must be a PDF or DOC file')
+            errors.add(:game_guide, 'must be a PDF or DOC file.')
+        end
+    end
+
+    def pictures_amount_ng_than_five
+        if  pictures.attachments.size > 5
+            pictures.purge
+            errors.add(:pictures, "can not be more than 5.")
         end
     end
 end
