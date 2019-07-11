98c2e5dabeee818018fc889c34643f4ade6f5639
Piotrek Karczewski <p.karczewsk@gmail.com>
2019-07-11 12:01:45 +0200

    mechanism for editing games

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -20,9 +20,26 @@ class GamesController < ApplicationController
     end
   end
 
+  def edit
+    @game = current_game
+  end
+
+  def update
+    @game = current_game
+    if @game.update_attributes(game_params)
+      redirect_to @game
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def game_params
     params.require(:game).permit(:title, :description, :rating, :game_guide, pictures: [])
+  end
+
+  def current_game
+    Game.find(params[:id])
   end
 end
