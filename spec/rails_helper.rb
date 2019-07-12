e890adaaf3bacc5ff494fe48c2832724727672e8
Piotrek Karczewski <p.karczewsk@gmail.com>
2019-07-12 13:48:13 +0200

    setup fixes

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,9 +1,3 @@
-Shoulda::Matchers.configure do |config|
-  config.integrate do |with|
-    with.test_framework :rspec
-    with.library :rails
-  end
-end
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
@@ -65,4 +59,11 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
   config.include FactoryBot::Syntax::Methods
+end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
 end
