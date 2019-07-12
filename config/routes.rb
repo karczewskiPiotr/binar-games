97ec6bf09ea61d72d09ba26f87ea53cfce018da3
Piotrek Karczewski <p.karczewsk@gmail.com>
2019-07-12 09:30:23 +0200

    fixed users routing

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users
   resources :users
-  root "users#index"
+  devise_scope :user do
+    root to: "devise/sessions#new"
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
