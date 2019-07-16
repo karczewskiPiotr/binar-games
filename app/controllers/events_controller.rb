414eb4a9c8b7bec28eee0be0f2c2fea9c105c0aa
RFoltynski <Foltynski.rafal@gmail.com>
2019-07-16 10:04:25 +0200

    add event functionality
    new controller created, new model created, posibility to add new events with title, description and date

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,38 @@
+class EventsController < ApplicationController
+    before_action :find_event, only: [:index, :show]
+    def index
+        @events = Event.all.order('created_at DESC')
+    end
+    def create
+        @event = Event.new(event_params)
+
+		if @event.save
+			redirect_to @event
+		else
+			render 'new'
+		end
+
+    end
+    def new
+        @event = Event.new
+    end
+    def edit
+
+    end
+    def show
+        
+    end
+    def update
+
+    end
+    def destroy
+
+    end
+    private
+    def event_params 
+        params.require(:event).permit(:title, :description, :event_time, :event_date)
+    end
+    def find_event
+        @event = Event.find(params[:id])
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_15_073704) do
+ActiveRecord::Schema.define(version: 2019_07_16_071615) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,15 @@ ActiveRecord::Schema.define(version: 2019_07_15_073704) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.time "event_time"
+    t.date "event_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
