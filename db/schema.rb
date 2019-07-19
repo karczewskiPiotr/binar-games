57f7ec24d34c68252c3a6be76a911b5a243eaf9f
RFoltynski <Foltynski.rafal@gmail.com>
2019-07-19 09:07:58 +0200

    private field added in events

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_18_123759) do
+ActiveRecord::Schema.define(version: 2019_07_19_070620) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2019_07_18_123759) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "owner_id"
+    t.boolean "private"
     t.index ["owner_id"], name: "index_events_on_owner_id"
   end
 
