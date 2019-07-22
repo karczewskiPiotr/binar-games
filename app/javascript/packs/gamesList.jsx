150437dbf5cf9fed95e9c730eda580303a2d3eb6
Piotrek Karczewski <p.karczewsk@gmail.com>
2019-07-22 14:16:29 +0200

    [add] user can search games by their name

--- app/javascript/packs/gamesList.jsx
+++ app/javascript/packs/gamesList.jsx
@@ -2,9 +2,14 @@ import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import axios from "axios";
 import Game from "../components/games/game";
+import Searchbar from "../components/games/searchbar";
 
 const GamesList = () => {
-  const [state, updateState] = useState({ games: [], loading: true });
+  const [state, updateState] = useState({
+    games: [],
+    loading: true,
+    searchPhrase: ""
+  });
 
   const compare = (a, b) => (b > a) - (b < a);
 
@@ -24,27 +29,41 @@ const GamesList = () => {
               compare(b.title.toLowerCase(), a.title.toLowerCase())
             );
           }),
-          loading: false
+          loading: false,
+          searchPhrase: ""
         });
       });
+  };
+
+  const handleSearch = searchPhrase => {
+    updateState(rest => {
+      return { ...rest, searchPhrase: searchPhrase };
+    });
+  };
+
+  const getGames = () => {
+    return state.searchPhrase
+      ? state.games.filter(game => {
+          return game.title.toLowerCase().search(state.searchPhrase) !== -1;
+        })
+      : state.games;
   };
 
   useEffect(fetchGames, []);
 
   return (
     <>
+      <Searchbar handleSearch={handleSearch} />
       <div className="row header">
         <div className="col-md">Title</div>
         <div className="col-md">Category</div>
         <div className="col-md rating">Rating</div>
       </div>
-      {state.games.map(game =>
-        state.loading ? (
-          "Loading"
-        ) : (
-          <Game key={game.id} fetchGames={fetchGames} game={game} />
-        )
-      )}
+      {state.loading
+        ? "Loading"
+        : getGames().map(game => (
+            <Game key={game.id} fetchGames={fetchGames} game={game} />
+          ))}
     </>
   );
 };
