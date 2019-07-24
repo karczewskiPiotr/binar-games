5aba6bb3b58911232fadf11535fb58717ade0e36
RFoltynski <Foltynski.rafal@gmail.com>
2019-07-24 09:17:22 +0200

    rubocop offenses fixed

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,8 +3,7 @@ class Event < ApplicationRecord
   belongs_to :owner, class_name: 'User'
   has_and_belongs_to_many :users
   validates :title, length: { minimum: 2, maximum: 35 }, presence: true
-  validates :description, length: { min: 2, maximum: 500 }, presence: true
+  validates :description, length: { maximum: 500 }, presence: true
   validates :event_time, presence: true
-  validates :event_date, presence: true 
-  
+  validates :event_date, presence: true
 end
