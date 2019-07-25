0a5eda8f500503f5631f4fb01e8285d254324978
Piotrek Karczewski <p.karczewsk@gmail.com>
2019-07-25 09:41:51 +0200

    [add] game details section

---
+++ app/javascript/components/games/gameDetails.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import Rating from "./Rating";
+import ImageZoom from "react-medium-image-zoom";
+
+const GameDetails = ({ visibility, game }) => {
+  return (
+    visibility && (
+      <>
+        <div className="game-details-wrapper">
+          <div className="row">
+            <div className="col-md description-wrapper">
+              <div className="description-header">
+                <h5>Description</h5>
+              </div>
+              <div className="description-content">{game.description}</div>
+            </div>
+            <div className="col-md-auto text-right user-rating-wrapper">
+              <div className="user-rating-header">
+                <h5>Your rating</h5>
+              </div>
+              <div className="user-rating">
+                <Rating rating={2} />
+              </div>
+            </div>
+          </div>
+          {game.pictures && (
+            <>
+              <div className="divider" />
+              <div className="pictures-header">
+                <h5>Pictures</h5>
+              </div>
+              <div className="row justify-content-start pictures">
+                {game.pictures.map((picture, index) => {
+                  return (
+                    <div key={index} className="col-md-auto text-center">
+                      <ImageZoom
+                        image={{
+                          src: picture.url,
+                          alt: `Game ${game.id} picture number ${index}`,
+                          className: "picture-miniature",
+                          style: { width: 128, height: 128 }
+                        }}
+                        zoomImage={{
+                          src: picture.url,
+                          alt: `Game ${
+                            game.id
+                          } picture number ${index} enlarged`
+                        }}
+                      />
+                    </div>
+                  );
+                })}
+              </div>
+            </>
+          )}
+          {game.guide && (
+            <>
+              <div className="divider" />
+              <div className="guide">
+                <a href={game.guide} target="_blank">
+                  Open game guide
+                </a>
+              </div>
+            </>
+          )}
+        </div>
+      </>
+    )
+  );
+};
+
+export default GameDetails;
