bfbc9c3a1977125ef0411782c6a77e2ff3a7b4f5
RFoltynski <Foltynski.rafal@gmail.com>
2019-07-26 06:45:34 +0200

    profile card improvments, mainly css changes

--- app/javascript/packs/userProfile.jsx
+++ app/javascript/packs/userProfile.jsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import axios from "axios";
 import cup from "../../../winner-cup.png";
 import star from "../../../star.png";
+import event from "../../../event.png";
+import ReactTooltip from "react-tooltip";
 
 class UserProfile extends Component {
   constructor(props) {
@@ -26,27 +28,44 @@ class UserProfile extends Component {
     return (
       <>
         <div className="profile">
+          <ReactTooltip />
           <h1 className="profile-header">Your Profile</h1>
           <div className="row profile-card-out">
             <div className="profile-card">
-              <img
-                className="profile-card-img"
-                src={this.state.users.avatar}
-                alt="user avatar"
-              />
+              <div className="profile-card-background">
+                <img
+                  className="profile-card-img"
+                  src={this.state.users.avatar}
+                  alt="user avatar"
+                />
+              </div>
               <div className="profile-card-nick">
                 <h2>{this.state.users.nick}</h2>
               </div>
               <div className="row">
-                <img className="profile-card-star" src={star} />
+                <img
+                  className="profile-card-star"
+                  src={star}
+                  data-tip="your points"
+                />
                 <div className="profile-card-points">
                   {" "}
                   : {this.state.users.points}{" "}
                 </div>
+                <img
+                  className="profile-card-cup"
+                  src={cup}
+                  data-tip="your ranking position"
+                />
+                <div className="profile-card-ranking">: 0</div>
               </div>
               <div className="row">
-                <img className="profile-card-cup" src={cup} />
-                <div className="profile-card-ranking">: 0</div>
+                <img
+                  className="profile-card-event"
+                  src={event}
+                  data-tip="your last few events"
+                />
+                <div className="profile-card-events" />
               </div>
             </div>
           </div>
