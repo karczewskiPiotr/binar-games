2d560687895cc4901d2a49713f25fc4e19ceea19
RFoltynski <Foltynski.rafal@gmail.com>
2019-07-27 09:37:36 +0200

    basic flip card functionality added

--- app/javascript/packs/userProfile.jsx
+++ app/javascript/packs/userProfile.jsx
@@ -2,13 +2,15 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import axios from "axios";
 import ReactTooltip from "react-tooltip";
+import ReactCardFlip from "react-card-flip";
 
 class UserProfile extends Component {
   constructor(props) {
     super(props);
     this.state = {
       users: [],
-      isLoding: false
+      isLoding: false,
+      isFlipped: false
     };
   }
 
@@ -23,13 +25,53 @@ class UserProfile extends Component {
       });
   }
 
+  handleClick = e => {
+    e.preventDefault();
+    this.setState({ isFlipped: !this.state.isFlipped });
+  };
+
   render() {
     return (
       <>
         <div className="profile-header">Your Profile</div>
-        <div className="profile" />;
+        <div className="profile">
+          {" "}
+          <ReactCardFlip
+            isFlipped={this.state.isFlipped}
+            flipSpeedBackToFront={0.1}
+            flipSpeedFrontToBack={0.1}
+          >
+            <div key="front">
+              <img
+                className="card-image"
+                src="//static.pexels.com/photos/59523/pexels-photo-59523.jpeg"
+              />
+              <button onClick={this.handleClick}>Flip Card</button>
+            </div>
+
+            <div key="back">
+              <img
+                className="card-image"
+                src="//img.buzzfeed.com/buzzfeed-static/static/2014-04/enhanced/webdr06/4/16/enhanced-11136-1396643149-13.jpg?no-auto"
+              />
+              <button onClick={this.handleClick}>Flip Card</button>
+            </div>
+          </ReactCardFlip>
+        </div>
       </>
     );
+  }
+}
+
+class Front extends Component {
+  render() {
+    return <div>Czosnek</div>;
+  }
+}
+
+class Back extends Component {
+  render() {
+    return <div> cebula </div>;
   }
 }
 
