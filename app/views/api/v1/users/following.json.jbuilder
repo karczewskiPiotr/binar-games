0cdc439d3445b46c069bf123c876c5cb43fc4251
Piotr Karczewski <34404855+karczewskiPiotr@users.noreply.github.com>
2019-07-30 10:11:06 +0200

    Revert "Favourites" (#36)

--- app/views/api/v1/users/following.json.jbuilder
+++
@@ -1,3 +0,0 @@
-json.data do
-  json.array! @followers, :id
-end
--- db/schema.rb
+++ db/schema.rb
@@ -85,16 +85,6 @@ ActiveRecord::Schema.define(version: 2019_07_25_083941) do
     t.index ["rating_user_id"], name: "index_ratings_on_rating_user_id"
   end
 
-  create_table "relationships", force: :cascade do |t|
-    t.integer "follower_id"
-    t.integer "followed_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["followed_id"], name: "index_relationships_on_followed_id"
-    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
-    t.index ["follower_id"], name: "index_relationships_on_follower_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
