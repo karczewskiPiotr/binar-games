a21a97d1306d089768b750fdc3f0fbf6c93b8cbb
Piotr Karczewski <34404855+karczewskiPiotr@users.noreply.github.com>
2019-07-30 13:59:56 +0200

    Deploy 30.07.2019 (#38)
    * routes set, cass and react pluged
    
    * avatar json fixed
    
    * profile page added
    
    * profile card improvments, mainly css changes
    
    * add user form improvments
    
    * user profile base, after cleaning
    
    * user profile - header added
    
    * basic flip card functionality added
    
    * added favourites feature in users ranking
    
    * profile-card-added
    
    * event form improved
    
    * tests added rubocop working, followers working, heart added
    
    * event form improved
    
    * rubocop fixed
    
    * review errors fixed
    
    * requested changes
    
    * fix
    
    * [fix]
    
    * newline fix
    
    * Revert "Favourites" (#36)
    
    * Animations (#35)
    
    * [add] games tab animations
    
    * [add] animations to users tab
    
    * merges and fixes (#37)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,21 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  def index; end
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(current_user.id)
+    return render 'devise/registrations/edit' unless @user.update_attributes(user_params)
+
+    bypass_sign_in @user
+    redirect_to users_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:nick, :email, :password, :password_confirmation, :id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :users
   resources :games
   resources :events , only: [:index, :create, :show, :new]
+  get "user_profile", to: 'users#user_profile'
 
   devise_scope :user do
     root to: "devise/sessions#new"
@@ -14,6 +15,7 @@ Rails.application.routes.draw do
       resources :categories, only: [:index, :show]
       resources :users, only: [:index]
       resources :events, only: [:index]
+      get '/users/current', to: 'users#current'
     end
   end
 end
