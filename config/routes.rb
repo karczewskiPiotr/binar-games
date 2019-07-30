7eb390071af637c3cb4d0f28a5f652ae82989a7c
mw.mysza@gmail.com <michalwalen97@gmail.com>
2019-07-30 10:29:21 +0200

    Revert "Revert "Favourites" (#36)"
    This reverts commit 0cdc439d3445b46c069bf123c876c5cb43fc4251.

--- config/routes.rb
+++ config/routes.rb
@@ -8,14 +8,20 @@ Rails.application.routes.draw do
   devise_scope :user do
     root to: "devise/sessions#new"
   end
-
+  
   namespace :api, defaults: { format: 'json' } do
     namespace :v1 do
       resources :games, only: [:index, :show, :update]
       resources :categories, only: [:index, :show]
-      resources :users, only: [:index]
+      resources :users, only: [:index] 
+      get 'users/current/following', to: 'users#following'
+      post 'users/current/following', to: 'users#follow'
+      post 'users/current/unfollow', to: 'users#unfollow'
       resources :events, only: [:index]
       get '/users/current', to: 'users#current'
     end
   end
+
+  resources :relationships,       only: [:create, :destroy]
+
 end
